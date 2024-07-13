e4b74055602cf073329198800e0dca37aab7b6eb
Duy Tu <duytu89@gmail.com>
2024-07-13 08:55:48 +0700

    dev

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,5 +1,6 @@
 import { UseGuards } from '@nestjs/common';
 import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import axios from 'axios';
 import { Server, Socket } from 'socket.io';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { RoomsService } from 'src/rooms/rooms.service';
@@ -9,7 +10,9 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
   private users: number = 0;
 
-  constructor(private roomsService: RoomsService) {}
+  constructor(private roomsService: RoomsService) {
+
+  }
 
   handleConnection(client: Socket) {
       this.users++;
@@ -35,13 +38,49 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @SubscribeMessage('joinRoom')
   handleJoinRoom(client: Socket, payload: { room: string, username: string }) {
       const room = this.roomsService.joinRoom(payload.room, payload.username);
-      client.join(room.name);
-      this.server.to(room.name).emit('userJoined', { room: room.name, username: payload.username });
+      console.log(room);
+      if(room){
+        client.join(room.name);
+        this.server.to(room.name).emit('userJoined', { room: room.name, username: payload.username });
+      }else{
+        this.server.emit('warning', {status:false});
+      }
   }
 
-//   @UseGuards(JwtAuthGuard)
+  //   @UseGuards(JwtAuthGuard)
   @SubscribeMessage('chat')
   handleMessage(client: Socket, payload: { room: string, sender: string, message: string }): void {
-      this.server.to(payload.room).emit('chat', payload);
+    this.server.to(payload.room).emit('chat', payload);
+    if(this.isJson(payload.message)){
+      const info = JSON.parse(payload.message);
+      if (info.status == 'out') {
+        this.sendData(payload.message);
+      }
+    }
+  }
+  async sendData(message: any) {
+    try {
+      console.log(message);
+      const rs = await axios.post(
+        'http://192.168.207.6:8088/check_device/store',
+        message,
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+      );
+      console.log(rs.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+   isJson(str) {
+      try {
+          JSON.parse(str);
+      } catch (e) {
+          return false;
+      }
+      return true;
   }
 }
